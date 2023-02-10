e21d1808ac96f8d3ea8366b8c271fcea027879f6
Firzyafriyan <124945253+Firzyafriyan@users.noreply.github.com>
2023-02-10 10:22:16 +0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author firzy afriyan
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+               
+        
+        char nilai;
+        Scanner input=new Scanner(System.in);
+        System.out.println("input nilai anda");
+                
+        
+        
+        // TODO code application logic here
+    }
+    
+}
+
